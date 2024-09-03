b8700da5d2942e6f7970d63722e36bfbc52e1e38
Robin S <robin.stake@elev.ga.ntig.se>
2024-09-03 10:41:01 +0200

    klar, ish

--- main.py
+++ main.py
@@ -10,6 +10,35 @@
 # - Längsta och kortaste ordet - om det finns flera, bestäm själv om du skriver ut ett eller flera
 # - Räkna antalet unika ord (alltså ord som bara förekommer en gång)
 
+def count_words(sentences):
+    total_words = 0
+    for sentence in sentences:
+        words = sentence.split(" ")
+        total_words += len(words)
+    return total_words
+
+def frequent_word(sentences):
+    from collections import Counter
+
+    words_frequency = Counter()
+    for sentence in sentences:
+        words = sentence.split()
+        words_frequency.update(word.lower() for word in words)
+
+    most_frequent = words_frequency.most_common(1)[0] #(1) är till för att hitta det MEST vanliga ordet och [0] är till för att ge listan ett start nummer
+    return most_frequent
+
+def average_word_length(sentences):
+    words = []
+    for sentence in sentences:
+        words.extend(sentence.split())
+    
+    if not words: #bara för att undvika division med 0 utifall att filen är tom
+        return 0
+    
+    total_length = sum(len(word) for word in words)
+    average_length = total_length / len(words)
+    return average_length
 
 def read_from_file(path: str):
     """Reads a file with the given parameter path and returns the file as a list of strings, split on newline (\n).
@@ -26,6 +55,15 @@ def read_from_file(path: str):
 def main():
     
     sentences = read_from_file("en_resa_genom_svenska_skogen.txt") # Här har du nu en lista av strängar från den inlästa filen.
+    
+    print(count_words(sentences))
+    
+    most_frequent = frequent_word(sentences)
+    print(most_frequent[0], most_frequent[1]) #printar både ordet och antalet 
+    
+    print(average_word_length(sentences))
 
 if __name__ == "__main__":
     main()
+
+#output fel: ger 438 istället för korrekt svar 432, vet inte varför.
